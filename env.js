a4a458c1a8f74768a6986f30faca0f35a7dff1f3
Sam <9622944+sam-sun00@users.noreply.github.com>
2023-11-06 15:06:04 -0800

    Update and rename env.example.js to env.js

---
+++ env.js
@@ -0,0 +1,28 @@
+window.settings = {
+  // 站点标题
+  title: '瑞克和莫蒂的时光机',
+  // 站点描述
+  description: 'Portal Gun',
+  // API
+  host: 'https://rickandmorty1.ca',
+  // 主题
+  theme: {
+    sidebar: 'dark',
+    header: 'dark',
+    color: 'black'
+  },
+  // 背景
+  // old background_url: 'https://images3.alphacoders.com/253/253399.png',
+  background_url: 'https://imagedelivery.net/_vfECU7V-UtjQAzmJaDbvg/aff1b4fc-a236-4dec-c6d2-c1ca9133ad00/usercontent',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
